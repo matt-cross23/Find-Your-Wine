74de07c5301ca8d73ccd8ce8aca96c56f2624e40
Matthew Cross <matthewcross1017@gmail.com>
2022-02-23 21:03:31 -0500

    change get

--- server/server.js
+++ server/server.js
@@ -23,19 +23,26 @@ app.use(express.urlencoded ({ extended: true}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use(errorHandler)
 
-// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+// Serve Frontend
+// if (process.env.NODE_ENV === 'production') {
+//     // Set build folder as static
+//     app.use(express.static(path.join(__dirname, '../client/build')))
   
-    // FIX: below code fixes app crashing on refresh in deployment
-    app.get('*', (_, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'))
-    })
-  } else {
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'))
-    })
-  }
+//     // FIX: below code fixes app crashing on refresh in deployment
+//     app.get('*', (_, res) => {
+//       res.sendFile(path.join(__dirname, '../client/build/index.html'))
+//     })
+//   } else {
+//     app.get('/', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/build/index.html'))
+//     })
+//   }
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
